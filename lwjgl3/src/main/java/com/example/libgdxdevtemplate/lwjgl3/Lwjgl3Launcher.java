1bceaa6984844d3afba6b16057db58faee155c30
shikeik <2667097598@qq.com>
2025-08-26 14:29:54 +0800

    修正遗漏lwjgl窗口名

--- lwjgl3/src/main/java/com/example/libgdxdevtemplate/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/example/libgdxdevtemplate/lwjgl3/Lwjgl3Launcher.java
@@ -22,7 +22,7 @@ public class Lwjgl3Launcher {
 
 	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
 		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-		configuration.setTitle("LibGDXBar-PVZLikeGame");
+		configuration.setTitle("LibgdxDevTemplate");
 		//// Vsync limits the frames per second to what your hardware can display, and helps eliminate
 		//// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
 		// 不使用垂直同步以避免窗口模式卡顿
